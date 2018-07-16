680040b7524d5dc86cd99c180860cee645dbc447
abi-wilson25 <agibailw@gmail.com>
2018-07-16 17:19:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 $(document).ready(function (){
 });
-$('.text').text('loading...');
-$.ajax({
-  type: "GET",
-  url:"https://api.meetup.com/2/cities",
-  success: function(data){
-  $('.text').text(JSON.stringify(data));
-},
-  dataType: 'jsonp',
-})
