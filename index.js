1373313fb033a7d24bd648148e9481fb9adb8678
abi-wilson25 <agibailw@gmail.com>
2018-07-16 17:21:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 $(document).ready(function (){
 });
+$('.btn').click(function() {
+  $('.text').text('Loading . . .');
+  $.ajax({
+    type:"GET",
+    url:"https://api.meetup.com/2/cities",
+    success: function(data) {
+      $('.text').text(JSON.stringify(data));
+    },
+    dataType: 'jsonp',
+  });
+});
