3a5a8973abe38c0b32583c30b4fbe175dfc5e9fb
abi-wilson25 <agibailw@gmail.com>
2018-07-16 17:28:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 $(document).ready(function (){
 });
+
+searchRepositories.onclick{
+  
+}
+$.get( "", function( data ) {
+  $( "body" )
+    .append( "Name: " + data.name ) // John
+    .append( "Time: " + data.time ); //  2pm
+}, "json" );
